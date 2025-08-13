47ebeba71a20f6fc22a8ffc34d432f7eac58c5d2
Christian Charbonnier <christian.charbonnier@estudiantes.utec.edu.uy>
2025-08-12 21:49:55 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
-divSaludo = document.getElementsByClassName("miDiv")[0]
+divSaludo = document.getElementById("miDiv");
+conbotonSaludar = document.getElementById("botonSaludar");
 
 divSaludo.addEventListener("click", function() {
     alert('Hola! Soy el div');
+});
+
+botonSaludar.addEventListener("click", function(event) {
+    event.stopPropagation();
+    alert('Hola!');
 });
